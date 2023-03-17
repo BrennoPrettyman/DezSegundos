b95369724ec0a7d63354fefc1ddf19eab3a2de31
DESKTOP-V0H394L\User <e107ti@etec.sp;gov;br>
2023-03-17 19:43:07 -0300

    Exercicio Resolvido

---
+++ Program.cs
@@ -0,0 +1,48 @@
+﻿namespace DezSegundos;
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.Clear();
+        Console.WriteLine("Contando até 10:");
+        Thread.Sleep(1000);
+
+        Console.Write("0");
+        Thread.Sleep(1000);
+
+        Console.Write(" 1");
+        Thread.Sleep(1000);
+
+        Console.Write(" 2");
+        Thread.Sleep(1000);
+
+        Console.Write(" 3");
+        Thread.Sleep(1000);
+
+        Console.Write(" 4");
+        Thread.Sleep(1000);
+
+        Console.Write(" 5");
+        Thread.Sleep(1000);
+
+        Console.Write(" 6");
+        Thread.Sleep(1000);
+
+        Console.Write(" 7");
+        Thread.Sleep(1000);
+
+        Console.Write(" 8");
+        Thread.Sleep(1000);
+
+        Console.Write(" 9");
+        Thread.Sleep(1000);
+
+        Console.Write(" 10");
+        Thread.Sleep(1000);
+
+        Console.WriteLine();
+
+        Console.WriteLine("Clique para continuar...");
+        Console.ReadKey();
+    }
+}
